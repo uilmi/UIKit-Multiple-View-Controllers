6a060900053b049ee708fc8bc9c6b2a94d580d9b
Ulul Ilmi <uilmi@na.edu>
2022-10-16 23:05:53 +0700

    init

---
+++ UIKit-Multiple-View-Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-Multiple-View-Controllers
+//
+//  Created by Ulul I. on 16/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
